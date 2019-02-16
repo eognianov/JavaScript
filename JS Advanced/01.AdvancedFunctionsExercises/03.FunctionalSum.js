63e38bf6819553a59134019511c0d0aaa7ae7f78
eognianov <emilian_89@abv.bg>
2019-02-16 11:27:43 +0200

    functional sum

---
+++ JS Advanced/01.AdvancedFunctionsExercises/03.FunctionalSum.js
@@ -0,0 +1,17 @@
+const add = (function(){
+    let sum = 0;
+
+    function add(num) {
+        sum += num;
+
+        return add;
+    }
+
+    add.toString = function (){
+        return sum;
+    }
+
+    return add;
+})();
+
+console.log(add(1)(2)(6));
