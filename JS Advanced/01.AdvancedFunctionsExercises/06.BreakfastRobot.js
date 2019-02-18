03952950c7720d700e519a689d279ab610cb46db
eognianov <emilian_89@abv.bg>
2019-02-18 20:01:31 +0200

    Problem 6 gix

--- JS Advanced/01.AdvancedFunctionsExercises/06.BreakfastRobot.js
+++ JS Advanced/01.AdvancedFunctionsExercises/06.BreakfastRobot.js
@@ -1,51 +1,63 @@
-const manager = (function () {
-    const ingredients = {
+const manager = (() => {
+    let elements = {
         protein: 0,
         carbohydrate: 0,
         fat: 0,
         flavour: 0
     };
-
-    const recipes = {
-        'apple': { carbohydrate: 1, flavour: 2 },
-        'coke': { carbohydrate: 10, flavour: 20 },
-        'burger': { carbohydrate: 5, fat: 7, flavour: 3 },
-        'omelet': { protein: 5, fat: 1, flavour: 1 },
-        'cheverme': { protein: 10, carbohydrate: 10, fat: 10, flavour: 10,   },
-    };
-
-    const prepareRecipe = (recipe, neededQuantity) => {
-        let neededIngs = Object.entries(recipes[recipe]);
-
-        for (let [ingrdient, quantity] of neededIngs) {
-            let store = ingredients[ingrdient] * neededQuantity;
-            if (quantity > store) {
-                return `Error: not enough ${ingrdient} in stock`;
+    return function (string) {
+        let prepOutput;
+        function errorPrint(str) {
+            prepOutput = `Error: not enough ${str} in stock`
+        }
+        function prepare(q, protein, carb, fat, flavour) {
+            let [pr, c, ft, fl] = [0, 0, 0, 0];
+            if (protein > 0) {
+                if (protein * q > elements.protein) return errorPrint('protein');
+                pr = q * protein;
             }
+            if (carb > 0) {
+                if (carb * q > elements.carbohydrate) return errorPrint('carbohydrate');
+                c = q * carb;
+            }
+            if (fat > 0) {
+                if (fat * q > elements.fat) return errorPrint('fat');
+                ft = q * fat;
+            }
+            if (flavour > 0) {
+                if (flavour * q > elements.flavour) return errorPrint('flavour');
+                fl = q * flavour
+            }
+            elements.protein -= pr;
+            elements.carbohydrate -= c;
+            elements.fat -= ft;
+            elements.flavour -= fl;
+            prepOutput = 'Success';
         }
-
-        for (let [ingrdient, quantity] of neededIngs) {
-            ingredients[ingrdient] -= neededQuantity * quantity;
-        }
-
-        return 'Success';
-    };
-
-    return function (input) {
-        let parts = input.split(' ').filter(x => x !== '');
-        let command = parts[0];
-
-        if (command === 'restock') {
-            let microElement = parts[1];
-            let quantity = Number(parts[2]);
-            ingredients[microElement] += quantity;
-            return 'Success';
-        } else if (command === 'prepare') {
-            let recipe = parts[1];
-            let quantity = Number(parts[2]);
-            return prepareRecipe(recipe, quantity);
-        } else if (command === 'report') {
-            return Object.entries(ingredients).map(x => `${x[0]}=${x[1]}`).join(' ');
+        let recipes = {
+            apple: (q) => prepare(q, 0, 1, 0, 2),
+            coke: (q) => prepare(q, 0, 10, 0, 20),
+            burger: (q) => prepare(q, 0, 5, 7, 3),
+            omelet: (q) => prepare(q, 5, 0, 1, 1),
+            cheverme: (q) => prepare(q, 10, 10, 10, 10)
+        };
+        let cmdArr = string.split(' ');
+        if (cmdArr.length === 1) {
+            return `protein=${elements.protein} carbohydrate=${elements.carbohydrate} ` +
+                `fat=${elements.fat} flavour=${elements.flavour}`
+        } else {
+            let cmd = cmdArr[0];
+            let quantity = cmdArr[2];
+            switch (cmd) {
+                case 'restock':
+                    let element = cmdArr[1];
+                    elements[element] += +quantity;
+                    return 'Success';
+                case 'prepare':
+                    let recipe = cmdArr[1];
+                    recipes[recipe](quantity);
+                    return prepOutput
+            }
         }
     }
 })();
