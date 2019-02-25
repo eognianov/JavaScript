1d9165572f27aae556b41227cb1b08182adb3b23
eognianov <emilian_89@abv.bg>
2019-02-25 21:54:04 +0200

    warehouse class

---
+++ JS Advanced/03.JSClassesExercises/09.Warehouse.test.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+const warehouse = require('./09.Warehouse');
+
+describe("TODO …", function() {
+    it("TODO …", function() {
+        // TODO: …
+    });
+    // TODO: …
+});
